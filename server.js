cf8c4322bb04cde9ad4ab51f2ba69905fc4013c8
parajuli rahul <parajulirahul98@gmail.com>
2023-12-04 11:47:47 +0545

    dubbing with bug and content add

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const authRoute = require("./router/auth-router");
 const contactRoute = require("./router/contact-router");
 const errorMiddleware = require("./middlewares/error-middleware");
 const connectDb = require("./utils/db");
 
+// handeling cors issue
+const corsOptions = {
+  origin: "http://localhost:3000",
+  method: "GET,POST,PUT,DELETE,PATCH,HEAD",
+  Credentials: true,
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+app.use(cors(corsOptions));
 app.use(express.json());
 
 //for route path api

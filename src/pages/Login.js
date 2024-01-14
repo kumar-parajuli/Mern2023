5c93053d1335d51b12a5eeb6ccee17dd5a2f2798
parajuli rahul <parajulirahul98@gmail.com>
2024-01-14 22:43:22 +0545

    login UI modified

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -52,87 +52,137 @@ const Login = () => {
   };
 
   return (
-    <div style={{ textAlign: "center", padding: "20px" }}>
-      <div style={{ marginBottom: "20px" }}>
-        {/* <div className="flex">
-          <img
-            src="/images/login.png"
-            alt="coding together"
-            width="400"
-            height="500"
-          />
-        </div> */}
-      </div>
-      <div
-        style={{
-          maxWidth: "400px",
-          margin: "auto",
-          backgroundColor: "#f8f8f8",
-          padding: "20px",
-          borderRadius: "8px",
-          boxShadow: "0 0 10px rgba(0, 0, 0, 0.1)",
-        }}>
-        <h1 style={{ marginBottom: "20px" }}>Login Form</h1>
-        <form onSubmit={handleSubmit}>
-          <label htmlFor="email">Email</label>
-          <input
-            name="email"
-            type="text"
-            id="email"
-            placeholder="Email"
-            required
-            autoComplete="off"
-            value={user.email}
-            onChange={handleInput}
-            // onChange={(e) => setUser({ ...user, email: e.target.value })}
-            style={{
-              width: "100%",
-              padding: "8px",
-              marginBottom: "10px",
-              boxSizing: "border-box",
-              borderRadius: "4px",
-              border: "1px solid #ccc",
-            }}
-          />
-          <br />
+    // <div style={{ textAlign: "center", padding: "20px" }}>
+    //   <div style={{ marginBottom: "20px" }}>
+    //     {/* <div className="flex">
+    //       <img
+    //         src="/images/login.png"
+    //         alt="coding together"
+    //         width="400"
+    //         height="500"
+    //       />
+    //     </div> */}
+    //   </div>
+    //   <div
+    //     style={{
+    //       maxWidth: "400px",
+    //       margin: "auto",
+    //       backgroundColor: "#f8f8f8",
+    //       padding: "20px",
+    //       borderRadius: "8px",
+    //       boxShadow: "0 0 10px rgba(0, 0, 0, 0.1)",
+    //     }}>
+    //     <h1 style={{ marginBottom: "20px" }}>Login Form</h1>
+    //     <form onSubmit={handleSubmit}>
+    //       <label htmlFor="email">Email</label>
+    //       <input
+    //         name="email"
+    //         type="text"
+    //         id="email"
+    //         placeholder="Email"
+    //         required
+    //         autoComplete="off"
+    //         value={user.email}
+    //         onChange={handleInput}
+    //         // onChange={(e) => setUser({ ...user, email: e.target.value })}
+    //         style={{
+    //           width: "100%",
+    //           padding: "8px",
+    //           marginBottom: "10px",
+    //           boxSizing: "border-box",
+    //           borderRadius: "4px",
+    //           border: "1px solid #ccc",
+    //         }}
+    //       />
+    //       <br />
 
-          <label htmlFor="password">Password</label>
-          <input
-            name="password"
-            type="password"
-            id="password"
-            placeholder="Password"
-            required
-            autoComplete="off"
-            value={user.password}
-            onChange={handleInput}
-            // onChange={(e) => setUser({ ...user, password: e.target.value })}
-            style={{
-              width: "100%",
-              padding: "8px",
-              marginBottom: "20px",
-              boxSizing: "border-box",
-              borderRadius: "4px",
-              border: "1px solid #ccc",
-            }}
-          />
-          <br />
+    //       <label htmlFor="password">Password</label>
+    //       <input
+    //         name="password"
+    //         type="password"
+    //         id="password"
+    //         placeholder="Password"
+    //         required
+    //         autoComplete="off"
+    //         value={user.password}
+    //         onChange={handleInput}
+    //         // onChange={(e) => setUser({ ...user, password: e.target.value })}
+    //         style={{
+    //           width: "100%",
+    //           padding: "8px",
+    //           marginBottom: "20px",
+    //           boxSizing: "border-box",
+    //           borderRadius: "4px",
+    //           border: "1px solid #ccc",
+    //         }}
+    //       />
+    //       <br />
 
-          <button
-            type="submit"
-            style={{
-              padding: "10px",
-              background: "#4caf50",
-              color: "#fff",
-              border: "none",
-              borderRadius: "4px",
-              cursor: "pointer",
-            }}>
-            Submit
-          </button>
-        </form>
-      </div>
-    </div>
+    //       <button
+    //         type="submit"
+    //         style={{
+    //           padding: "10px",
+    //           background: "#4caf50",
+    //           color: "#fff",
+    //           border: "none",
+    //           borderRadius: "4px",
+    //           cursor: "pointer",
+    //         }}>
+    //         Submit
+    //       </button>
+    //     </form>
+    //   </div>
+    // </div>
+    <>
+      <section>
+        <main>
+          <div className="section-registration">
+            <div className="container grid grid-two-cols">
+              <div className="registration-image reg-img">
+                <img
+                  src="/images/register.png"
+                  alt="a nurse with a cute look"
+                  width="400"
+                  height="500"
+                />
+              </div>
+              {/* our main login code  */}
+              <div className="registration-form">
+                <h1 className="main-heading mb-3">Login form</h1>
+                <br />
+                <form onSubmit={handleSubmit}>
+                  <div>
+                    <label htmlFor="email">Email</label>
+                    <input
+                      type="text"
+                      name="email"
+                      value={user.email}
+                      onChange={handleInput}
+                      placeholder="email"
+                    />
+                  </div>
+
+                  <div>
+                    <label htmlFor="password">Password</label>
+                    <input
+                      type="password"
+                      name="password"
+                      value={user.password}
+                      onChange={handleInput}
+                      placeholder="password"
+                    />
+                  </div>
+                  <br />
+                  <button type="submit" className="btn btn-submit">
+                    Login Now
+                  </button>
+                </form>
+              </div>
+            </div>
+          </div>
+        </main>
+      </section>
+    </>
   );
 };
 
